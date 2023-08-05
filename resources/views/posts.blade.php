8c07679f2800778beadc8fd9baf57af4b2e952ee
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-05 11:05:37 -0500

    Blade: the absolute basics

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,20 +4,18 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <?php foreach ($posts as $post) : ?>
+    @foreach ($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
 
-    <article>
-        <h1>
-            <a href="/posts/<?= $post->slug; ?>">
+            </h1>
 
-                <?= $post->title ?>
-            </a>
-
-        </h1>
-
-        <div>
-            <?= $post->excerpt ?>
-        </div>
-    </article>
-    <?php endforeach ?>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
 </body>
