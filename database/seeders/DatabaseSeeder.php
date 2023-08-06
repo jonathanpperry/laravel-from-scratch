5707df0d512e52e75103d2db982f4d45d64a0ba4
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-06 17:55:10 -0500

    Turbo boost with factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,58 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // User::truncate();
+        // Post::truncate();
+        // Category::truncate();
 
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
+        $user = User::factory()->create([
+            'name' => 'John Doe',
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My First Post',
-            'slug' => 'my-first-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>',
-
-        ]);
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>',
-
-        ]);
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>',
-
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
-}
+} 
