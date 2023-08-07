147191a34decaa29a4820dce76ffbeda78fb9ca9
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-06 22:51:43 -0500

    Convert the HTML and CSS to blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,19 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {!! $post->title !!}
-                </a>
+    @include ('_posts-header')
 
-            </h1>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
 
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a
-                    href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
 
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        </article>
-    @endforeach
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+
+        </div>
+    </main>
 </x-layout>
