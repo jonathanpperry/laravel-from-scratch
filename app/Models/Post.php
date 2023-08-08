62550a0efefb401935f2766aba0d06b5b4591a2c
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-08 10:16:38 -0500

    Author filtering

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,8 +19,11 @@ class Post extends Model
             ->orWhere('body', 'like', '%'.$search.'%'));
 
         $query->when($filters['category'] ?? false, fn ($query, $category) => $query->whereHas('category', fn ($query) => $query->where('slug', $category))
-
         );
+
+        $query->when($filters['author'] ?? false, fn ($query, $author) => $query->whereHas('author', fn ($query) => $query->where('username', $author))
+    );
+
 
     }
 
