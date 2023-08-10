49d20c03a4d155f552b0d740d66272b9e0d1c9c6
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-10 14:43:49 -0500

    Failed validation and old input data

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -11,7 +11,11 @@
                     </label>
 
                     <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name"
-                        required>
+                        value="{{ old('name') }}" required>
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
 
                 </div>
                 <div class="mb-6">
@@ -20,7 +24,11 @@
                     </label>
 
                     <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username"
-                        required>
+                        value="{{ old('username') }}" required>
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
 
                 </div>
                 <div class="mb-6">
@@ -29,7 +37,11 @@
                     </label>
 
                     <input class="border border-gray-400 p-2 w-full" type="text" name="email" id="email"
-                        required>
+                        value="{{ old('email') }}" required>
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
 
                 </div>
                 <div class="mb-6">
@@ -40,12 +52,24 @@
                     <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password"
                         required>
 
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
                 </div>
                 <div class="mb-6">
                     <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
                         Submit
                     </button>
                 </div>
+
+                {{-- @if ($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif --}}
             </form>
         </main>
     </section>
