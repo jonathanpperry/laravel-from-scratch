b7a9de2af11a047bec0d7dafbf100264ebcab414
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-14 14:57:21 -0500

    54: Make the comments section dynamic

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,18 +1,21 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 p-6 border border-gray-600 rounded-xl space-x-4">
     <div class="flex-shrink-0">
         {{-- User avatar --}}
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl" />
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60"
+            class="rounded-xl" />
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">
                 Posted
-                <time> 8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
         <p>
-            This is the text of a comment.
+            {{ $comment->body }}
         </p>
     </div>
 </article>
