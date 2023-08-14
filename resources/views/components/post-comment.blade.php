c3c7e680ba8e0d9234c114b34a8211dc8968f4ff
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-14 11:12:31 -0500

    52: Write the markup for a post comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,18 @@
+<article class="flex bg-gray-100 p-6 border border-gray-600 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        {{-- User avatar --}}
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl" />
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time> 8 months ago</time>
+            </p>
+        </header>
+        <p>
+            This is the text of a comment.
+        </p>
+    </div>
+</article>
