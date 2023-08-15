cef32c3eb06d57ec5cf36f789cd9feed3e8064d1
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-15 15:30:56 -0500

    56: Activate the comment form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -4,7 +4,7 @@
     <article class="flex space-x-4">
         <div class="flex-shrink-0">
             {{-- User avatar --}}
-            <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60"
+            <img src="https://i.pravatar.cc/60?u={{ $comment->user_id }}" alt="" width="60" height="60"
                 class="rounded-xl" />
         </div>
         <div>
@@ -12,7 +12,7 @@
                 <h3 class="font-bold">{{ $comment->author->username }}</h3>
                 <p class="text-xs">
                     Posted
-                    <time>{{ $comment->created_at }}</time>
+                    <time>{{ $comment->created_at->format('F j, Y, g:i a') }}</time>
                 </p>
             </header>
             <p>
