d98847ccc46ee1a6f3a803dd924dcef35b0b315a
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-17 12:53:43 -0500

    64: Validate and store post thumbnails

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,9 +29,9 @@ class PostController extends Controller
 
     public function store()
     {
-
         $attributes = request()->validate([
             'title' => 'required',
+            'thumbnail' => 'required|image',
             'slug' => ['required', Rule::unique('posts', 'slug')],
             'excerpt' => 'required',
             'body' => 'required',
@@ -39,6 +39,7 @@ class PostController extends Controller
         ]);
 
         $attributes['user_id'] = auth()->id();
+        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
 
         Post::create($attributes);
 
