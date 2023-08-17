082874d5765f1c24c29257fdd00bc7a716155bae
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-17 15:31:51 -0500

    66: Extend the admin layout

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,11 +1,11 @@
-@props(['name', 'type' => 'text'])
+@props(['name'])
 
 <x-form.field>
     {{-- Label --}}
     <x-form.label name="{{ $name }}" />
 
-    <input class="border border-gray-400 p-2 w-full" type="{{ $type }}" name="{{ $name }}"
-        id="{{ $name }}" value="{{ old($name) }}" required>
+    <input class="border border-gray-200 p-2 w-full rounded" name="{{ $name }}"
+        id="{{ $name }}" value="{{ old($name) }}" required {{ $attributes }}>
 
     {{-- Error --}}
     <x-form.error name="{{ $name }}" />
